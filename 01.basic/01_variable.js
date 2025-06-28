a5561b3686657bfa826ed979a2aef8e64dc45ffc
ankitbronax <btech10178.22@bitmesra.ac.in>
2025-06-28 06:00:33 +0000

    datatype

---
+++ 01.basic/01_variable.js
@@ -0,0 +1,9 @@
+let accountEmail="ankitgta420@gmail.com"
+var accountCity="ranchi"
+accountCollege="bit mesra"/*aisa bhi decleare hota h but dont do that*/
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountCity)
+console.table([accountEmail,accountCity,accountCollege])
---
+++ 01.basic/02_datatype.js
@@ -0,0 +1,7 @@
+let animal="lion"
+let vores="carnivores"
+let height="2 meter"
+let weight ="400-500 kg"
+
+
+console.log(typeof "weight")
