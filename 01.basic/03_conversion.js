6d2a35e43ee99be249a55aa5c8d7333a871d4a8c
ankitbronax <btech10178.22@bitmesra.ac.in>
2025-06-28 06:44:11 +0000

    conversion

---
+++ 01.basic/03_conversion.js
@@ -0,0 +1,15 @@
+let score="34"
+let marks=45
+
+let valueInNumber=Number(score)
+
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(typeof score)
+console.log(typeof marks)
+console.log(typeof valueInNumber)
+console.log(typeof isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn);
